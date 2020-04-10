fddee31e81b5bc29e286169122dacb68ff7a5e5d
Maximiler Arouca <maximiler@gmail.com>
2020-04-10 18:22:34 -0300

    Configuring Apollo Server in React

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+  uri: 'https://spotify-graphql-server.herokuapp.com/graphql',
+});
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <ApolloProvider client={client}>
+      <h1>App</h1>
+    </ApolloProvider>
+  );
 }
 
 export default App;
